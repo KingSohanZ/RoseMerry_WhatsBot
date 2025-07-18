cf2458d0a46fb68f9d03598d4d72a115d50c84f7
RoseMerry <130471209+KingSohanZ@users.noreply.github.com>
2025-07-18 19:14:04 +0530

    Create ttwm.js

---
+++ plugins/ttwm.js
@@ -0,0 +1,10 @@
+module.exports.run = async (sock, m, args) => {
+    const jid = m.key.remoteJid;
+    const url = args[0];
+    if (!url) return sock.sendMessage(jid, { text: "❗ Invalid watermark video link." });
+
+    await sock.sendMessage(jid, {
+        video: { url },
+        caption: "🎥 TikTok Video (With Watermark)"
+    });
+};
